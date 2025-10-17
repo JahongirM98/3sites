86439179e4be73fc5e1ffc31d0e4209a85af2a78
Jahongir Mirhalikov <3690969006@my.lerna.tj>
2025-10-17 14:05:42 +0500

    fix template path

--- services/sugdpark_site/catalog_sugd/views.py
+++ services/sugdpark_site/catalog_sugd/views.py
@@ -4,8 +4,8 @@ from .models import Category, Product
 def product_list(request):
     categories = Category.objects.all().order_by("name")
     products = Product.objects.select_related("category").all().order_by("name")
-    return render(request, "catalog/product_list.html", {"categories": categories, "products": products})
+    return render(request, "pages/product_list.html", {"categories": categories, "products": products})
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    return render(request, "catalog/product_detail.html", {"product": product})
+    return render(request, "pages/product_detail.html", {"product": product})
